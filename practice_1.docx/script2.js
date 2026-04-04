db2751080153e2e440f7fbc3f523264bdc5a06ca
codewithnill <nbnill.hmlnmh@gmail.com>
2026-04-04 23:45:22 +0600

    commented this unnecessary line 'const upper_username = username.value.toUpperCase();'

--- practice_1.docx/script2.js
+++ practice_1.docx/script2.js
@@ -3,6 +3,6 @@
 const username = document.getElementById("username");
 // const upper_username = username.toUpperCase();
 username.addEventListener("input", function() {
-    const upper_username = username.value.toUpperCase();
+    //const upper_username = username.value.toUpperCase();
     this.value = this.value.toUpperCase();
 });
