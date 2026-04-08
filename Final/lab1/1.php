3b1e52df1813a77fcddd80691197434359307f9a
codewithnill <nbnill.hmlnmh@gmail.com>
2026-04-08 16:44:25 +0600

    Area and perimeter.

---
+++ Final/lab1/1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>1</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php
+    print("Task 1<br>");
+    $length=10;
+    $width=20;
+    print("Length : {$length}<br>");
+    print("Width : {$width}<br>");
+    $area=$length * $width;
+    $perimeter=2*($length + $width);
+    print("Area : {$area}<br>");
+    print("Perimeter : {$perimeter}");
+?>
