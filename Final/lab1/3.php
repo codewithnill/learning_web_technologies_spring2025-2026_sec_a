4a102baea905f02a3d2ee4ab8db655e05fdde279
codewithnill <nbnill.hmlnmh@gmail.com>
2026-04-08 16:58:16 +0600

    even and odd.

---
+++ Final/lab1/3.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>3</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php
+    print("Task 3<br>");
+    $num1=22;
+    $num2=23;
+    print("Let num1 : {$num1},");
+    print(" and num2 : {$num2}<br>");
+    
+    if($num1%2==0) {
+        print("{$num1} is even<br>");
+    } else {
+        print("{$num1} is odd<br>");
+    }
+
+
+    if($num2%2==0) {
+        print("{$num2} is even<br>");
+    } else {
+        print("{$num2} is odd<br>");
+    }
+    
+?>
