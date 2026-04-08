58d8a0dc6d758cd399e0f6c68d7672130c2c76a3
codewithnill <nbnill.hmlnmh@gmail.com>
2026-04-08 16:53:04 +0600

    vat amount.

---
+++ Final/lab1/2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php
+    print("Task 2<br>");
+    $amount=100;
+    print("Amount : {$amount}<br>");
+    print("Let vat is equal to 15%.<br>");
+    $v=15;
+    $vat=100*($v/100);
+    print("Vat amount : {$vat}<br>");
+    
+?>
