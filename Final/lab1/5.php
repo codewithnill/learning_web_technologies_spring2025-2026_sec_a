338a19ce5e9a585036dd0607aa803cadfd6988fe
codewithnill <nbnill.hmlnmh@gmail.com>
2026-04-09 13:10:58 +0600

    all the odd numbers between 10 to 100.

---
+++ Final/lab1/5.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>5</title>
+</head>
+<body>
+    <?php
+   
+    $num = 11; 
+
+    echo "Odd numbers between 10 and 100 are: <br>";
+
+    while ($num <= 100) {
+        echo $num . " ";
+        $num += 2; 
+    }
+    
+?>
+</body>
+</html>
