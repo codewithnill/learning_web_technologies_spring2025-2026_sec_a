c6dc5f850cdc726ed1b77ccac315c159d33e8758
codewithnill <nbnill.hmlnmh@gmail.com>
2026-04-15 21:18:37 +0600

    php file for 1A.

--- Final/lab2/1/A/A.php
+++ Final/lab2/1/A/A.php
@@ -1,26 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>1A</title>
-</head>
-<body>
-    <form action="" method="POST">
-        <br><br>
-        <fieldset>
-            <legend>Name</legend>
-            <label for="name">Enter your name:</label>
-            <input type="text" name="name">
-            <hr>
-            
-            <input type="submit" value="Submit">
-        </fieldset>
-    </form>
-
-    <?php
-    // Check if the form was submitted
-    
-    ?>
-</body>
-</html>
+<?php
+    if(isset($_POST['name'])){
+        $name = $_REQUEST['name'];
+        
+        if($name == ""){
+            echo "Name is empty!";
+        }else{
+            echo "Submitted Name: " . $name;
+        }
+    }else{
+        echo "No name submitted. Please go back and enter a name.";
+    }
+?>
