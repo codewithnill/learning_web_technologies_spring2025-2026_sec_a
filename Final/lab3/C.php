c971cb0eeacfa760d4d000e0b4cd765c8a044c03
codewithnill <nbnill.hmlnmh@gmail.com>
2026-04-22 16:49:00 +0600

    login page

---
+++ Final/lab3/C.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <a href="A.php">Home | </a>
+    <a href="https://www.example.com">Login |</a>
+    <a href="B.php">Registration</a>
+    <br><br>
+
+    <fieldset>
+        <legend><strong>Login</strong></legend>
+        <label>Username : </label><input type="text" name="name" placeholder="username"><br><hr>
+        <label>Password : </label><input type="password" name="password" placeholder="password"><br><hr>
+    </fieldset>
+        
+        <hr>
+        <button>Submit</button>
+
+    </fieldset>
+
+    <h5>Copyright ©2017</h5>
+</body>
+</html>
+
+<?php
+    // echo "Welcome to X company."; 
+?>
