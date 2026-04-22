62f7af52c9f054674159b4fd0177aae38cc413ad
codewithnill <nbnill.hmlnmh@gmail.com>
2026-04-22 16:04:55 +0600

    Public home page

---
+++ Final/lab3/A.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Public home</title>
+</head>
+<body>
+    <a href="https://www.example.com">Home | </a>
+    <a href="https://www.example.com">Login |</a>
+    <a href="B.php">Registration</a>
+    <br><br>
+
+    <h2>Welcome to X company.</h2>
+    <h5>Copyright ©</h5>
+</body> 
+</html>
+
+<?php
+    // echo "Welcome to X company."; 
+?>
