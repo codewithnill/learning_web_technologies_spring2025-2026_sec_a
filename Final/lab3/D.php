70b0e64ebecbe91fe26cf47aec63b22fafe13fec
codewithnill <nbnill.hmlnmh@gmail.com>
2026-04-22 17:03:08 +0600

    Forgot password page.

---
+++ Final/lab3/D.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forgot password</title>
+</head>
+<body>
+    <fieldset>
+        <legend>Forgot Password</legend>
+        <label>Enter email : </label><input type="email" name="email"><br><hr>
+        <button>Submit</button>
+    </fieldset>
+    
+</body>
+</html>
+
+<?php
+    // echo "Welcome to X company."; 
+?>
