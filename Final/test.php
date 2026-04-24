45cea2afb24b05d6ff321c0f17dc02445aa93b75
codewithnill <nbnill.hmlnmh@gmail.com>
2026-04-24 08:18:52 +0600

    Moved all lab folders and practice files into Mid folder

---
+++ Final/test.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test php</title>
+</head>
+<body>
+    <h1>
+        Test
+    </h1>
+</body>
+</html>
+
+<?php
+    $name="Nill"; // all primitive data types exist
+    // $id=12;
+    // $cgpa=3.5;
+    // // $std=[1,'NB',2.5];
+    // $std=array(1,'NB',4);
+    // $std[2];
+
+    // $stds = [
+    //     [1,'NB',2.5];
+    //     [1,'NB',2.5];
+    //     [1,'NB',2.5]
+    // ];
+
+    // $std[2][2];
+
+    // // associative array
+    // $std=['id'=>1,'name'=>'NB','cgpa'=>2.5];
+    // $std['cgpa'];
+
+    // // numeric array
+    // $stds = [
+    // // associative array
+    //     // ['id'=>1,'name'=>'NB','cgpa'=>2.5],
+    //     // ['id'=>2,'name'=>'xyz','cgpa'=>2.3],
+    //     // ['id'=>3,'name'=>'abc','cgpa'=>2.3]
+
+    //     's1'=>['id'=>1,'name'=>'NB','cgpa'=>2.5],
+    //     's2'=>['id'=>2,'name'=>'xyz','cgpa'=>2.3],
+    //     's3'=>['id'=>3,'name'=>'abc','cgpa'=>2.3]
+    // ];
+
+    // // $stds[2]['cgpa'];
+    // $stds['s2']['name'];
+
+    // $std=[1,'NB',2.5];
+    // for($a=0;$a<count($std);$a++) {
+    //     $std[$a];
+    // }
+
+    // $std=['id'=>1,'name'=>'NB','cgpa'=>2.5];
+    // foreach($std as $v) {
+    //     $v;
+    // }
+
+    // if() {
+
+    // } else {
+
+    // }
+
+    echo "Hello PHP";
+    echo "Hello PHP {$name} another test...";
+    
+    print("Your name is : {$name}");
+    
+?>
+
+<h1>
+        Test
+    </h1>
