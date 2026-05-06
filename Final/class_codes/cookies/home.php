417c452c4e7ca23b72f8b5a6c41b69b5774121c0
codewithnill <nbnill.hmlnmh@gmail.com>
2026-05-06 16:18:48 +0600

    previously it was just "if(!isset($_COOKIE['status']))". Now, checking session super global also for username index; "  if(!isset($_COOKIE['status']) || !isset($_SESSION['username'])) ".

---
+++ Final/class_codes/cookies/home.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+    if(!isset($_COOKIE['status'])){
+        header('location: login.php');
+        exit();
+    }
+?>
+
+<html>
+    <head>
+        <title>Home Page</title>
+    </head>
+    <body>
+            <h1>Welcome Home  <?php echo $_SESSION['username'];?></h1>
+            <a href="logout.php">Logout</a>
+    </body>
+</html>
