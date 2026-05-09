1b723ec5b01f58c6254a8aecbdde3be56e3da5c5
codewithnill <nbnill.hmlnmh@gmail.com>
2026-05-09 08:44:30 +0600

    sign up processing.

---
+++ Final/class_codes/user_management/controller/signup_check.php
@@ -0,0 +1,24 @@
+<?php
+    session_start();
+    require_once('../model/user_model.php');
+    if(isset($_POST['submit'])) {
+        $username = $_REQUEST['username'];
+        $password = $_REQUEST['password'];
+        $email = $_REQUEST['email'];
+
+        if($username == "" || $password == "" || $email == ""){
+            echo "null username/password/email";
+        }else{
+            $user = ['username'=> $username, 'password' => $password, 'email'=> $email];
+            $status = addUser($user);
+            if($status) {
+                header('location: ../view/login.php');
+            } else {
+                header('location: ../view/signup.php');
+            }
+
+        }
+    } else {
+        header('location: ../view/signup.php');
+    }
+?>
