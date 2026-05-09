4e1b756b281410fff986389510f12c5ae1005f61
codewithnill <nbnill.hmlnmh@gmail.com>
2026-05-09 14:59:37 +0600

    login anchor tag and session codes removed, and h1 tag removed.

--- Final/class_codes/user_management/view/edit.php
+++ Final/class_codes/user_management/view/edit.php
@@ -1,16 +1,22 @@
 <?php
     session_start();
-    $id = $_GET['id'];
-    $users = $_SESSION['users'];
-    $user =[];
-    foreach($users as $u){
-        if($id == $u['id']){
-            $user = $u;
-            //$u['username'] = $username; 
-            //$u['email'] = $email; 
-            break;
-        }
+    if(!isset($_COOKIE['status'])){
+        header('location: login.php');
+        exit();
     }
+    require_once('../model/user_model.php');
+    $id = $_GET['id'];
+    $user = getUserById($id); // ✅ Fetch from database
+    // $users = $_SESSION['users']; // ❌ Uses session, not database
+    // $user =[];
+    // foreach($users as $u){
+    //     if($id == $u['id']){
+    //         $user = $u;
+    //         //$u['username'] = $username; 
+    //         //$u['email'] = $email; 
+    //         break;
+    //     }
+    // }
 ?>
 
 <!DOCTYPE html>
@@ -19,8 +25,6 @@
     <title>Edit User</title>
 </head>
 <body>
-
-        <h1>List of User </h1>
         <a href="user_list.php">Back</a> |
         <a href="../controller/logout.php">Logout</a> 
         <br>
@@ -28,12 +32,12 @@
         <form method="POST" action="../controller/update_check.php" enctype="">
             <fieldset>
                 <legend>Edit User</legend>
-            ID: <input type="text" name="id" readonly value="<?=$user['id']?>"/> <br>
-            Username: <input type="text" name="username" value="<?=$user['username']?>"/> <br>
-            Email: <input type="email" name="email" value="<?=$user['email']?>"/> <br>
+                ID: <input type="text" name="id" readonly value="<?php echo $user['id']; ?>"/> <br>
+                Username: <input type="text" name="username" value="<?php echo $user['username']; ?>"/> <br>
+                Email: <input type="email" name="email" value="<?php echo $user['email']; ?>"/> <br>
                     <input type="submit" name="submit" value="Update"/>
             </fieldset>
         </form>
-        <a href="login.php">login</a>
+        <!-- <a href="login.php">login</a> -->
 </body>
 </html>
