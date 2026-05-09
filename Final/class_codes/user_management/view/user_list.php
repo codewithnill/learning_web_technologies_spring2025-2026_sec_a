06601b100bd41bad6064900266484776d2e54d38
codewithnill <nbnill.hmlnmh@gmail.com>
2026-05-09 08:46:19 +0600

    user list with tables.

---
+++ Final/class_codes/user_management/view/user_list.php
@@ -0,0 +1,50 @@
+<?php
+    session_start();
+    // $users =[
+    //     ['id'=>1, 'username'=>'alamin', 'email'=>'alamin@aiub.edu'],
+    //     ['id'=>2, 'username'=>'xyz', 'email'=>'xyz@aiub.edu'],
+    //     ['id'=>3, 'username'=>'abc', 'email'=>'alamin@aiub.edu'],
+    //     ['id'=>4, 'username'=>'pqr', 'email'=>'alamin@aiub.edu'],
+    //     ['id'=>5, 'username'=>'xyz', 'email'=>'alamin@aiub.edu'],
+    // ];
+    require_once('../model/user_model.php');
+    
+    $users = getAllUser();
+
+    $_SESSION['users']=$users;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+        <h1>List of User </h1>
+        <a href="home.php">Back</a> |
+        <a href="../controller/logout.php">Logout</a> 
+        <br>
+
+        <table border=1>
+            <tr>
+                <th>ID</th>
+                <th>USERNAME</th>
+                <th>EMAIL</th>
+                <th>ACTION</th>
+            </tr>
+        <?php foreach($users as $user){   ?>
+            <tr>
+                <td><?php echo $user['id'];?></td>
+                <td><?=$user['username']?></td>
+                <td><?=$user['email']?></td>
+                <td>
+                    <a href='edit.php?id=<?=$user['id']?>'> EDIT </a> |
+                    <a href='../controller/delete.php?id=<?= $user['id'] ?>'> DELETE </a> |
+                    <a href='details.php'> DETAILS </a>
+                </td>
+            </tr>
+
+        <?php } ?>
+        </table>
+</body>
+</html>
