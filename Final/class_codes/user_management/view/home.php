c48a7b959a99e1f697b277fdbc81d01ffc8051f9
codewithnill <nbnill.hmlnmh@gmail.com>
2026-05-09 08:37:10 +0600

    home page, with a login check functionality right at the top.

---
+++ Final/class_codes/user_management/view/home.php
@@ -0,0 +1,19 @@
+<?php
+    session_start();
+    if(!isset($_COOKIE['status'])){
+        header('location: login.php');
+        exit();
+    }
+?>
+
+    <html>
+        <head>
+            <title>Home Page</title>
+        </head>
+        <body>
+                <h1>Welcome Home  <?php echo $_SESSION['username'];?></h1>
+                <a href="user_list.php">User List</a> |
+                <a href="../controller/logout.php">Logout</a> 
+        </body>
+        </html>
+
