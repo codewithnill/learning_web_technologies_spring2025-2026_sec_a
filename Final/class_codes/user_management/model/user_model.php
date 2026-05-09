c6c32faa3b4d27f2a7643c3deb9947e774646557
codewithnill <nbnill.hmlnmh@gmail.com>
2026-05-09 14:58:59 +0600

    one comment added.

--- Final/class_codes/user_management/model/user_model.php
+++ Final/class_codes/user_management/model/user_model.php
@@ -10,13 +10,14 @@
         $sql = "select * from users where username='{$user['username']}' and password='{$user['password']}'";
         $result = mysqli_query($con, $sql);
 
-        if(mysqli_num_rows($result) == 1) {
+        if(mysqli_num_rows($result) == 1) { // the user found
             return true;
         } else {
             return false;
         }
     }
 
+    
     function addUser($user) {
         $con = getConnection();
         $sql = "insert into users values (null, '{$user['username']}', '{$user['password']}', '{$user['email']}')";
