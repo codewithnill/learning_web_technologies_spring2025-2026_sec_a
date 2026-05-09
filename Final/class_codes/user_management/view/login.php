3a83d627169d036d566a07bb585c2e36e6d1e86b
codewithnill <nbnill.hmlnmh@gmail.com>
2026-05-09 08:38:03 +0600

    login page.

---
+++ Final/class_codes/user_management/view/login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Form Submission</title>
+</head>
+<body>
+        <form method="POST" action="../controller/login_check.php" enctype="">
+            Username: <input type="text" name="username" value=""/> <br>
+            Password: <input type="password" name="password" value=""/> <br>
+                    <input type="submit" name="submit" value="Submit"/>
+        </form>
+        <a href="signup.php">signup</a>
+</body>
+</html>
