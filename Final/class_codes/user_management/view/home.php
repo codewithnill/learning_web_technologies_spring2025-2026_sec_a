07e263a5f7aac7849b43997278d6e3121918600f
codewithnill <nbnill.hmlnmh@gmail.com>
2026-05-09 09:52:23 +0600

    indentation maintained.

--- Final/class_codes/user_management/view/home.php
+++ Final/class_codes/user_management/view/home.php
@@ -6,14 +6,14 @@
     }
 ?>
 
-    <html>
-        <head>
-            <title>Home Page</title>
-        </head>
-        <body>
-                <h1>Welcome Home  <?php echo $_SESSION['username'];?></h1>
-                <a href="user_list.php">User List</a> |
-                <a href="../controller/logout.php">Logout</a> 
-        </body>
-        </html>
+<html>
+    <head>
+        <title>Home Page</title>
+    </head>
+    <body>
+            <h1>Welcome Home  <?php echo $_SESSION['username'];?></h1>
+            <a href="user_list.php">User List</a> |
+            <a href="../controller/logout.php">Logout</a> 
+    </body>
+</html>
 
